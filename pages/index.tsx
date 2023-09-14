a872b8a3e8954846340e1f8c52e5510b40a756cf
SteakhouseGroup <128934133+SteakhouseGroup@users.noreply.github.com>
2023-09-14 19:07:26 +0100

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,22 @@
-import { ConnectWallet } from "@thirdweb-dev/react";
-import styles from "../styles/Home.module.css";
-import Image from "next/image";
 import { NextPage } from "next";
+import { Heading, VStack, Text, Image, SimpleGrid, Divider } from "@chakra-ui/react";
 
 const Home: NextPage = () => {
   return (
-    <div></div>
+    <VStack bg="dw2" h="100vh" textAlign={"center"} m="auto" justifyContent={"center"} p={4} textColour="dw2">
+      <Heading>Welcome to Dragon Warriors!</Heading>
+      <Text>Claim royalties today for either of our collections</Text>
+      <Divider />
+      <SimpleGrid columns={{base:2, lg:5}} m="auto" spacing="2">
+      <Image src="/DragonWarrior1.jpeg" height="70px"  width="70px" borderRadius="md"/>
+      <Image src="/DragonWarrior2.jpeg" height="70px"  width="70px" borderRadius="md"/>
+      <Divider />
+
+      <Image src="/DragonWarrier3.jpeg" height="70px"  width="70px" borderRadius="md"/>
+      <Image src="/DragonWarrior4.jpeg" height="70px"  width="70px" borderRadius="md"/>
+      <Divider />
+      </SimpleGrid>
+      </VStack>
   );
 };
 
